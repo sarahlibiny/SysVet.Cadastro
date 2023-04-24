72da5a2d8210ded76b0a425add78b91ea926d9d9
sarah libiny <libinysarah@gmail.com>
2023-04-24 18:08:23 -0300

    criando entidades animal e tutor na infra

---
+++ SysVet.Cadastro.Infra/Data/Entidades/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysVet.Cadastro.Infra.Data.Entidades
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Raca { get; set; }
+        public DateOnly DataNascimento { get; set; }
+        public int TutorId { get; set; }
+    }
+}
