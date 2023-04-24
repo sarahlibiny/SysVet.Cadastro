24dfa43856a5002c37f8441005bd8069ec8ec040
sarah libiny <libinysarah@gmail.com>
2023-04-24 17:26:36 -0300

    criando entidades animal e tutor no dominio

---
+++ SysVet.Cadastro.Dominio/Entidades/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace SysVet.Cadastro.Dominio.Entidades
+{
+    public class Animal
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Raca { get; private set; }
+        public DateOnly DataNascimento { get; private set; }
+        public int TutorId { get; private set; }
+    }
+}
---
+++ SysVet.Cadastro.Dominio/Entidades/Tutor.cs
@@ -0,0 +1,8 @@
+﻿namespace SysVet.Cadastro.Dominio.Entidades
+{
+    public class Tutor
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+    }
+}
