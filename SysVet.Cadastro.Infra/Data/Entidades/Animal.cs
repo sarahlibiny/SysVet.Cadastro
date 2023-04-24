8924fbac78a9670ebb753f533d18183f62b5604e
sarah libiny <libinysarah@gmail.com>
2023-04-24 18:11:19 -0300

    removendo usings desnecessarios

--- SysVet.Cadastro.Infra/Data/Entidades/Animal.cs
+++ SysVet.Cadastro.Infra/Data/Entidades/Animal.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SysVet.Cadastro.Infra.Data.Entidades
+﻿namespace SysVet.Cadastro.Infra.Data.Entidades
 {
     public class Animal
     {
